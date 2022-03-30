4464b9f64662ab68284c68b5955afb9a8d1fdcff
Frank Agnello <fja5533@rit.edu>
2022-03-30 00:27:36 -0400

    this is now fully functional, it will eat resource

--- IaC/docker.sh
+++ IaC/docker.sh
@@ -5,4 +5,12 @@ sudo amazon-linux-extras install docker
 echo "Docker installed successfully!"
 sudo service docker start
 sudo usermod -a -G docker ec2-user
+echo "About to enter loop"
+while true
+do
+	sleep 30s
+    d=$(date +%Y-%m-%d-%H-%M-%S)
+    docker run jrottenberg/ffmpeg -i "https://s53.nysdot.skyvdn.com/rtplive/R4_090/chunklist_w673018897.m3u8" -vframes 1 -q:v 2 -f image2pipe - | aws s3 cp - s3://ffmpeg-tests/$d.jpg
+    echo "Took picture"
+done
 --//--
